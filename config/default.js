c8fc390959fb4819cae5dda12d8178e269529fab
oakfire <oakfire@163.com>
2015-08-31 08:15:50 +0000

    YAC

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 /** if changed conifg, restart process to enable new config */
 module.exports = {    
     /** default port */
-    port: 3000,
-    //host: ,
+    port: 3001,
+    host: '127.0.0.1',
     /** silent console or not */
-    console_quiet: false,
+    console_quiet: true,
     /** log level, can be "silly" "debug" "verbose" "info" "warn" "error" */
     log_level: 'info',
     /** log path, default: logs */
