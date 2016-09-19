d58999f61d191a9d29f8033cc87fa77329b66603
oakfire <oakwatcher@gmail.com>
2016-09-19 08:54:41 +0000

    Unixtime page

--- routers/tools.js
+++ routers/tools.js
@@ -12,16 +12,35 @@ var toolsInfo = [
         name: 'ASCII 编码表',
         path: 'ASCII',
     },
+    {
+        name: 'UNIX 时间戳转换',
+        path: 'unixtime',
+    },
 ];
+
+function checkTool(path) {
+    for(var i = 0; i < toolsInfo.length; ++i){
+        if(toolsInfo[i].path == path) {
+           return true;
+        }
+    }
+    return false; 
+}
 
 router.get('/', function onToolsIndex(req, res) {
     var renderData = { toolsInfo: toolsInfo };
     res.render('tools', renderData);  
 });
 
-router.get('/:toolname', function onToolContent(req, res){
-    var renderData = { toolsInfo: toolsInfo };
+router.get('/:toolname', function onToolContent(req, res, next){
     var toolname = req.params.toolname;
+    if(!checkTool(toolname)){
+        return next();
+    }
+    var renderData = { 
+        toolsInfo: toolsInfo,
+        toolname: toolname,
+    };
     res.render('tools/' + toolname, renderData);
 });
 
